54df813812b15884f0eb247a17f52e659e45d6f5
Abdalazez-Azmi <90723420+Abdalazez-Azmi@users.noreply.github.com>
2021-11-18 21:01:23 +0200

    edit

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 let values = document.getElementsByClassName("values");
 let winner = document.getElementById("winner");
-let audio = new Audio('victory.mp3')
+let audio = new Audio("victory.mp3");
+let counts = document.getElementById("counts");
+let x_1 = 0;
+let o_1 = 0;
+const x = ()=>{
+  counts.innerHTML = `${x_1 ++} - ${o_1}`;
+}
+const o = ()=>{
+  counts.innerHTML = `${x_1} - ${o_1 ++ }`;
+}
+window.onload = x()
+window.onload = o()
 
+const mute = () => {
+  audio = "";
+};
+
+const again = () => {
+  for (let i = 0; i < values.length; i++) {
+    values[i].innerHTML = "";
+  }
+};
 
 const game = () => {
-  event.target.innerHTML = 'x'
-if(event.target.innerHTML = 'x'){
-  event.target.innerHTML = 'o'
-}
-
-
-
- 
-  
+  if ((event.target.innerHTML = "x")) {
+    event.target.innerHTML = "o";
+  }
 
   if (
     (values[0].innerHTML === "x" &&
@@ -43,9 +57,14 @@ if(event.target.innerHTML = 'x'){
       values[7].innerHTML === "x" &&
       values[8].innerHTML === "x")
   ) {
-    // console.log(event.target.innerHTML)
     winner.innerHTML = " The Winner is x";
-    audio.play()
+    audio.play();
+    x()
+    setTimeout(function () {
+      again();
+      winner.innerHTML = "";
+    }, 4000);
+
   } else if (
     (values[0].innerHTML === "o" &&
       values[1].innerHTML === "o" &&
@@ -76,9 +95,14 @@ if(event.target.innerHTML = 'x'){
       values[8].innerHTML === "o")
   ) {
     winner.innerHTML = " The Winner is O";
-    audio.play()
-  }else{
-    winner.innerHTML= 'No one win'
+    o()
+    setTimeout(function () {
+      again();
+      winner.innerHTML = "";
+    }, 4000);
+    audio.play();
+  } else {
+    winner.innerHTML = "No one win";
   }
-
 };
+
